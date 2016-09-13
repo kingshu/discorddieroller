d0ddcd0f859b0b998291f135139317fdfd1db541
Krishnabh Medhi <kingshu.crusader@gmail.com>
2016-09-12 21:33:16 -0700

    Prevents breaking when begins with /r but no match

--- dieroller.js
+++ dieroller.js
@@ -6,41 +6,43 @@ bot.on("message", msg => {
         var myRegexp = /(\d+)d(\d+)(\s*(\+|\-)\s*(\d+))?/i ;
         var match = myRegexp.exec(msg.content);
 
-        var dieCount = match[1];
-        var dieFaces = match[2];
+        if (match && match[1] && match[2]) {
+            var dieCount = match[1];
+            var dieFaces = match[2];
 
-        if (parseInt(dieCount) > 200) {
-            msg.channel.sendMessage("That's too many dice, "+msg.author+". Please roll fewer");
-            
-        }
-        else if (parseInt(dieFaces) > 200) {
-            msg.channel.sendMessage("That die has an impossibly huge number of faces, "+msg.author+". Maybe roll a smaller die?");
-        }
-        else {
-            var result = "Roll: ";
-            var total = 0;
-            for (var i=1; i<=parseInt(dieCount); i++) {
-                var rollResult = getRandomInt(parseInt(dieFaces));
-                result += rollResult;
-                if (i<parseInt(dieCount)) {
-                    result += " + "
-                }
-                total += rollResult;
+            if (parseInt(dieCount) > 200) {
+                msg.channel.sendMessage("That's too many dice, "+msg.author+". Please roll fewer");
+                
             }
-
-            if (match[5]!=undefined) {
-                result += "  *("+match[4]+match[5]+")*";
-                if (match[4]=="+") {
-                    total += parseInt(match[5]);
-                }
-                else {
-                    total -= parseInt(match[5]);
-                }
+            else if (parseInt(dieFaces) > 200) {
+                msg.channel.sendMessage("That die has an impossibly huge number of faces, "+msg.author+". Maybe roll a smaller die?");
             }
+            else {
+                var result = "Roll: ";
+                var total = 0;
+                for (var i=1; i<=parseInt(dieCount); i++) {
+                    var rollResult = getRandomInt(parseInt(dieFaces));
+                    result += rollResult;
+                    if (i<parseInt(dieCount)) {
+                        result += " + "
+                    }
+                    total += rollResult;
+                }
 
-            result += "\nResult :  **"+total+"**";
+                if (match[5]!=undefined) {
+                    result += "  *("+match[4]+match[5]+")*";
+                    if (match[4]=="+") {
+                        total += parseInt(match[5]);
+                    }
+                    else {
+                        total -= parseInt(match[5]);
+                    }
+                }
 
-            msg.channel.sendMessage(result);
+                result += "\nResult :  **"+total+"**";
+
+                msg.channel.sendMessage(result);
+            }
         }
     }
 });
